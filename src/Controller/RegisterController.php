8c67fe4f5e5aea28dd79ac6235e8be3561b71ccb
Sebastien-Lechat <seb_le@hotmail.fr>
2021-12-28 11:53:03 +0100

    push pour adrien

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -17,7 +19,7 @@ class RegisterController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function register(Request $request): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
         # Nouvelle instance de la classe user (entity)
         $user = new User();
@@ -33,7 +35,23 @@ class RegisterController extends AbstractController
             # Récupération des données du formulaire
             $user = $form->getData();
 
+            # On set la valeur de createdAt
             $user->setCreatedAt(new DateTime());
+
+            # On set la valeur du role
+            $user->setRoles(['ROLE_USER']);
+
+            # On set la valeur du password en hash pour qu'il ne soit pas affiché en clair dans la base de données
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            # Création du conteneur et insertion en base de données grâce à Doctrine et l'outil entityManager.
+            $entityManager->persist($user);
+
+            # On vide l'entity manager des données précédement contenues.
+            $entityManager->flush();
+
+            # Redirection sur la page de connexion après l'inscription
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('register/register.html.twig', [
